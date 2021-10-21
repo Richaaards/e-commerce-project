abbc9492b399e89981be9f1f7e3e1e01a2f52bc4
Richard Phonthibsvads <richardphonthibsvads@MacBook-Pro-de-Richard.local>
2021-10-21 11:56:02 +0200

    Use Open Sans Condensed typography & add hover effect to MenuItem

--- e-commerce-clothing/src/components/MenuItem/MenuItem.component.jsx
+++ e-commerce-clothing/src/components/MenuItem/MenuItem.component.jsx
@@ -3,7 +3,12 @@ import "./MenuItem.style.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={`${size} menu-item`}>
+      {/* Need the following div for the image effect on hover */}
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
