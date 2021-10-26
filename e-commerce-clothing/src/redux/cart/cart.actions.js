e99897c6e18ecd4c402dbaf8214dad39b4965d5d
Richard Phonthibsvads <richardphonthibsvads@MacBook-Pro-de-Richard.local>
2021-10-26 12:56:14 +0200

    add item reducer to cart from CollectionItem component

--- e-commerce-clothing/src/redux/cart/cart.actions.js
+++ e-commerce-clothing/src/redux/cart/cart.actions.js
@@ -1,6 +1,11 @@
+import MenuItem from "../../components/MenuItem";
 import { cartActionTypes } from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: cartActionTypes.TOGGLE_CART_HIDDEN,
 });
 
+export const addItem = (item) => ({
+  type: cartActionTypes.ADD_ITEM,
+  payload: item,
+});
